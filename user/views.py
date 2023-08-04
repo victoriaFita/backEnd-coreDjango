d4f6ba88f0e9344684c44a59bbfb8a691fb1d2af
Augusto Balsanelli <augustebalsanel@proton.me>
2023-08-04 09:08:40 -0300

    Fixed JWT

--- user/views.py
+++ user/views.py
@@ -7,3 +7,10 @@ from .serializers import UserSerializer
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import AllowAny
+
+class LoginView(TokenObtainPairView):
+    permission_classes = (AllowAny,)
+
