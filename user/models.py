972f26906c657f50702fa7818f4c22a1141219cc
Augusto Balsanelli <github.891d8@passfwd.com>
2023-08-17 15:22:00 -0300

    Added ViaCEP API for user extra info

--- user/models.py
+++ user/models.py
@@ -1,3 +1,5 @@
+import requests
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -13,12 +15,32 @@ class User(AbstractUser):
     data_nascimento = models.DateField(
         _("Birth Date"), auto_now=False, auto_now_add=False, blank=True, null=True
     )
+    cep = models.CharField(_("CEP"), max_length=8, blank=True, null=True)
+    logradouro = models.CharField(_("Street"), max_length=200, blank=True, null=True)
+    complemento = models.CharField(_("Complement"), max_length=200, blank=True, null=True)
+    bairro = models.CharField(_("Neighborhood"), max_length=200, blank=True, null=True)
+    localidade = models.CharField(_("City"), max_length=200, blank=True, null=True)
+    uf = models.CharField(_("State"), max_length=2, blank=True, null=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
     EMAIL_FIELD = "email"
 
     objects = CustomUserManager()
+
+    def save(self, *args, **kwargs):
+        if self.cep:
+            url = f"https://viacep.com.br/ws/{self.cep}/json/"
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = response.json()
+                self.logradouro = data.get("logradouro")
+                self.complemento = data.get("complemento")
+                self.bairro = data.get("bairro")
+                self.localidade = data.get("localidade")
+                self.uf = data.get("uf")
+        super().save(*args, **kwargs)
+        
 
     def __str__(self):
         return self.email
