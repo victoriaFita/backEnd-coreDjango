8c858a0f1eac9df3831f54eb3e806836767e24ea
HericMD <hericmdms@gmail.com>
2023-08-18 09:00:18 -0300

    Deleted Services model and related

--- assistance/serializers/service.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-
-from assistance.models import Service
-
-class ServiceSerializer(ModelSerializer):
-    class Meta:
-        model = Service
-        fields = "__all__"
