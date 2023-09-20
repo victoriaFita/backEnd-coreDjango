6f56a74d82e55ca768eea201bab3252086ba1400
Augusto Balsanelli-Verediana <github.891d8@passfwd.com>
2023-09-20 04:29:37 -0300

    Fixed API errors

--- assistance/serializers/equipment.py
+++ assistance/serializers/equipment.py
@@ -4,8 +4,26 @@ from assistance.models import Equipment
 from uploader.serializers.image import ImageSerializer
 
 class EquipmentSerializer(ModelSerializer):
-    image = ImageSerializer() 
+    image = ImageSerializer(required=False) 
 
     class Meta:
         model = Equipment
         fields = "__all__"
+
+    def update(self, instance, validated_data):
+        # Atualizando campos normais
+        instance.name = validated_data.get('name', instance.name)
+        instance.model = validated_data.get('model', instance.model)
+        # ... (atualize os outros campos conforme necessário)
+
+        # Atualizando a imagem
+        image_data = validated_data.pop('image', None)
+        if image_data:
+            image_serializer = ImageSerializer(instance.image, data=image_data, partial=True)
+            if image_serializer.is_valid():
+                image_serializer.save()
+            else:
+                raise serializers.ValidationError(image_serializer.errors)
+        
+        instance.save()
+        return instance
