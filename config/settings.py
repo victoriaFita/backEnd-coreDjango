d193775cd1bb892b4434e4b5b7d522c6c432845f
Augusto Balsanelli-Verediana <github.891d8@passfwd.com>
2023-09-21 18:51:37 -0300

    Fixed some bugs

--- config/settings.py
+++ config/settings.py
@@ -61,7 +61,6 @@ if MODE == "PRODUCTION":
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
