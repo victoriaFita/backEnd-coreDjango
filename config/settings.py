e83135c29fd8f94bcbc737a8200cfaff8549a212
Augusto Balsanelli-Verediana <github.891d8@passfwd.com>
2023-09-21 17:47:42 -0300

    Added .env and improvements

--- config/settings.py
+++ config/settings.py
@@ -10,25 +10,22 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from dotenv import load_dotenv
 import os
 from pathlib import Path
 from datetime import timedelta
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+load_dotenv()
+
+print("SECRET_KEY:", os.getenv("SECRET_KEY"))
+print("DEBUG:", os.getenv("DEBUG"))
+print("ALLOWED_HOSTS:", os.getenv("ALLOWED_HOSTS"))
+print("MODE:", os.getenv("MODE"))
+
+MODE = os.getenv("MODE")
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9tn5yoh+(e*!0*sot8p=4!9rl7dulpvr2bu*ft$nk%(jp9l53t"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
-
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = os.getenv("DEBUG", "False")
 
 # Application definition
 
